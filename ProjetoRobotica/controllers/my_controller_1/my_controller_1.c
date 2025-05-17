98cdbf0769145f689c82491d6e315b8af324361d
Alan Daiki Suga <110938759+daikisuga@users.noreply.github.com>
2025-05-17 16:18:31 -0300

    robo anda aleatoriamente e danca

--- ProjetoRobotica/controllers/my_controller_1/my_controller_1.c
+++ ProjetoRobotica/controllers/my_controller_1/my_controller_1.c
@@ -11,14 +11,14 @@
 #define NUM_SENSORS 8
 #define MAX_SPEED 6.28
 #define NUM_BOXES 20
+#define THRESHOLD_MOV 0.005  // tolerância para detectar movimento da caixa
+#define ESPERA_MOVIMENTO 60  // passos de simulação (~2s)
 
 int main() {
-  int i;
   wb_robot_init();
+  srand(time(NULL));
 
-  srand(time(NULL)); // Para movimentos aleatórios
-
-  // Dispositivos
+  // Motores
   WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
   WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
   wb_motor_set_position(left_motor, INFINITY);
@@ -26,80 +26,100 @@ int main() {
   wb_motor_set_velocity(left_motor, 0);
   wb_motor_set_velocity(right_motor, 0);
 
+  // Sensores de proximidade
   WbDeviceTag prox[NUM_SENSORS];
   char sensor_name[5];
-  for (i = 0; i < NUM_SENSORS; i++) {
+  for (int i = 0; i < NUM_SENSORS; i++) {
     sprintf(sensor_name, "ps%d", i);
     prox[i] = wb_robot_get_device(sensor_name);
     wb_distance_sensor_enable(prox[i], TIME_STEP);
   }
 
-  // Supervisor
-  WbNodeRef self = wb_supervisor_node_get_self();
-
-  // Caixas
+  // Referência para caixas
   WbNodeRef caixas[NUM_BOXES];
-  const double *caixa_pos_ant[NUM_BOXES];
+  double caixa_pos_ant[NUM_BOXES][3];
   char def[16];
-  for (i = 0; i < NUM_BOXES; i++) {
+  for (int i = 0; i < NUM_BOXES; i++) {
     sprintf(def, "CAIXA%d", i + 1);
     caixas[i] = wb_supervisor_node_get_from_def(def);
-    caixa_pos_ant[i] = (caixas[i]) ? wb_supervisor_node_get_position(caixas[i]) : NULL;
+    const double *pos = (caixas[i]) ? wb_supervisor_node_get_position(caixas[i]) : NULL;
+    if (pos) {
+      caixa_pos_ant[i][0] = pos[0];
+      caixa_pos_ant[i][1] = pos[1];
+      caixa_pos_ant[i][2] = pos[2];
+    }
   }
 
-  // Movimento aleatório inicial
+  // Movimento inicial aleatório
   double v_left = ((double)(rand() % 100) / 100.0) * MAX_SPEED;
   double v_right = ((double)(rand() % 100) / 100.0) * MAX_SPEED;
   wb_motor_set_velocity(left_motor, v_left);
   wb_motor_set_velocity(right_motor, v_right);
 
-  bool colidiu = false;
-  int timer = 100; // tempo andando aleatoriamente
+  int timer = 100;
 
   while (wb_robot_step(TIME_STEP) != -1) {
-    // Lê sensores
+    printf("Timer: %d\n", timer);
+
+    // Verifica colisão
     int bateu = 0;
-    for (i = 0; i < NUM_SENSORS; i++) {
+    for (int i = 0; i < NUM_SENSORS; i++) {
       double val = wb_distance_sensor_get_value(prox[i]);
       if (val > 80.0) bateu = 1;
     }
 
-    if (!colidiu && bateu) {
-      colidiu = true;
+    if (bateu) {
       printf("COLIDIU!\n");
 
-      // Espera 1 segundo para caixa se mover
-      int t;
-      for (t = 0; t < 30; t++) wb_robot_step(TIME_STEP);
+      // Espera antes de verificar movimento
+      for (int t = 0; t < ESPERA_MOVIMENTO; t++) wb_robot_step(TIME_STEP);
 
-      for (i = 0; i < NUM_BOXES; i++) {
-        if (!caixas[i] || !caixa_pos_ant[i]) continue;
+      // Verifica se alguma caixa se moveu
+      int dancou = 0;
+      for (int i = 0; i < NUM_BOXES; i++) {
+        if (!caixas[i]) continue;
+
         const double *nova_pos = wb_supervisor_node_get_position(caixas[i]);
-
         if (nova_pos && (
-            fabs(nova_pos[0] - caixa_pos_ant[i][0]) > 0.01 ||
-            fabs(nova_pos[2] - caixa_pos_ant[i][2]) > 0.01)) {
-          // A caixa se moveu
+              fabs(nova_pos[0] - caixa_pos_ant[i][0]) > THRESHOLD_MOV ||
+              fabs(nova_pos[2] - caixa_pos_ant[i][2]) > THRESHOLD_MOV)) {
+
           printf("Caixa %d se moveu! DANÇA!\n", i + 1);
 
           // Dança: gira no próprio eixo
-          for (int j = 0; j < 100; j++) {
+          while(true) {
             wb_motor_set_velocity(left_motor, MAX_SPEED);
             wb_motor_set_velocity(right_motor, -MAX_SPEED);
             wb_robot_step(TIME_STEP);
           }
+
+          dancou = 1;
           break;
         }
       }
 
-      // Para após dançar
-      wb_motor_set_velocity(left_motor, 0);
-      wb_motor_set_velocity(right_motor, 0);
-      break;
+      if (!dancou) {
+        printf("Nenhuma caixa se moveu.\n");
+      }
+
+      // Atualiza posições antigas
+      for (int i = 0; i < NUM_BOXES; i++) {
+        if (caixas[i]) {
+          const double *pos = wb_supervisor_node_get_position(caixas[i]);
+          if (pos) {
+            caixa_pos_ant[i][0] = pos[0];
+            caixa_pos_ant[i][1] = pos[1];
+            caixa_pos_ant[i][2] = pos[2];
+          }
+        }
+      }
+
+      // Reinicia timer e movimento aleatório
+      timer = 0;
     }
 
-    // Anda aleatoriamente no começo
-    if (!colidiu && timer-- == 0) {
+    // Movimento aleatório a cada N passos
+    if (timer-- <= 0) {
       v_left = ((double)(rand() % 100) / 100.0) * MAX_SPEED;
       v_right = ((double)(rand() % 100) / 100.0) * MAX_SPEED;
       wb_motor_set_velocity(left_motor, v_left);
